f5e1bd4eddab6feee419b95b4d12e627a44a7c55
josefernandezramos <actionreplaychuni@gmail.com>
2025-02-20 12:29:23 +0100

    corregir error

--- services/read.py
+++ services/read.py
@@ -13,7 +13,7 @@ def registre():
             "name": "Josep Oriol",
             "surname": "Roca",
             "age": 23
-        }
+        },
         "users3": {
             "id": 3,
             "name": "Juan Manuel",
