fd7c2893d4d996457fe3fc7daeac8b74e5f00c14
dev <dev@introprg.myguest.virtualbox.org>
2025-02-20 12:01:07 +0100

    read acabat

---
+++ services/read.py
@@ -0,0 +1,26 @@
+form schema import read_sch
+
+def registre():
+        users = {
+            "user1":{
+                "id":1
+                "name":"Roger",
+                "surname":"Sobrino",
+                "age":49
+            },
+            "user2":{
+                "id":2,
+                "name":"Josep Oriol",
+                "surname":"Roca",
+                "age": 23
+            },
+            "user3":{
+                "id":3,
+                "name":"Juan Manuel",
+                "surname":"Roca"
+                "age": 24
+            }
+
+        }
+
+        return read_sch.schemas(users)
