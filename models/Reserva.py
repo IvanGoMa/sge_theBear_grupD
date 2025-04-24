c3e9c460faca14e3a4f599fd2bde79c33dd96cdf
IvanGoMa <99ivangm@gmail.com>
2025-04-24 08:03:13 +0200

    a

--- models/Reserva.py
+++ models/Reserva.py
@@ -1,10 +1,11 @@
 from sqlmodel import SQLModel, Field
 
 class Reserva(SQLModel, table = True):
-    id: int = Field(default=None, primary_key=True)
-    date: date
-    time: time
-    id_table:int = (foreign_key=True)
     id_client: int = (foreign_key=True)
+    id_mesa: int = (foreign_key=True)
+    dia: int
+    hora: int
+
+
 
 
