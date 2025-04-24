bb698d69af362ec2bdcb96c5fe109a8a107ba822
hannita <hnnzpa@gmail.com>
2025-04-24 12:07:19 +0200

    Fetes les tables: Jornada, Mesa i Data

--- models/Data.py
+++ models/Data.py
@@ -1,5 +1,7 @@
 from sqlmodel import SQLModel, Field
 
 class Data(SQLModel, table = True):
+    any: int = Field(primary_key=True)
+    mes: int = Field(primary_key=True)
     dia: int = Field(primary_key=True)
     hora: int = Field(primary_key=True)
--- models/Jornada.py
+++ models/Jornada.py
@@ -3,7 +3,8 @@ from sqlmodel import SQLModel, Field
 
 class Jornada(SQLModel, table=True):
     id_empleat: int = Field(foreign_key="empleat.id", primary_key=True)
-    dia_inici: int = Field(foreign_key="data.dia", primary_key=True)
-    hora_inici: int = Field(foreign_key="data.hora", primary_key=True)
-    dia_fi: int = Field(foreign_key="data.dia")
+    dia: int  = Field(foreign_key="data.dia", primary_key=True)
+    mes: int = Field(foreign_key="data.mes", primary_key=True)
+    any: int = Field(foreign_key="data.any", primary_key=True)
+    hora_inici: int = Field(foreign_key="data.dia", primary_key=True)
     hora_fi: int = Field(foreign_key="hora.dia")
