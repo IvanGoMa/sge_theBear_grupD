3171c1c0c996508a8e639f327fc83ac48fa1c292
josefernandezramos <actionreplaychuni@gmail.com>
2025-05-08 12:41:17 +0200

    crear endpoints empleat

--- services/empleat_service.py
+++ services/empleat_service.py
@@ -1,11 +1,18 @@
-from schema.empleats_sch import empleats_schema
 from sqlmodel import  Session, select
 from models.Empleat import Empleat
+from schema.empleats_sch import empleats_schema
+from schema.empleats_sch import empleat_schema
+
 
 def get_all_empleats(db:Session):
     sql_read = select(Empleat)
     empleats = db.exec(sql_read).all()
     return empleats_schema(empleats)
+
+def get_empleat( id, db:Session):
+    sql_read = select(Empleat).where(Empleat.id == id)
+    empleats = db.exec(sql_read).one()
+    return empleat_schema(empleats)
 
 def add_new_empleat(nom_complet:str, telefono:int, cargo:str, db:Session):
     db_empleat = Empleat(nom_complet=nom_complet, telefono=telefono, cargo=cargo)
