96ff41015bc6682fa1d3ea2ce769ac5f8110c6b1
hannita <hnnzpa@gmail.com>
2025-05-08 12:33:05 +0200

    Fets els schema de Mesa, Jornada, fets els serveis de Mesa i Jornada i 2 endpoints de Jornada

--- models/Data.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class Data(SQLModel, table = True):
-    any: int = Field(primary_key=True)
-    mes: int = Field(primary_key=True)
-    dia: int = Field(primary_key=True)
-    hora: int = Field(primary_key=True)
